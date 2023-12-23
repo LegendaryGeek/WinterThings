6fc37b9f71a6d2f2d66d262528c8392af286b4cd
LegendaryGeek <timothydaley4@gmail.com>
2023-12-23 09:45:01 -0500

    fixed block entity constructor

--- src/main/java/geek/winterthings/entity/BaseBlockEntity.java
+++ src/main/java/geek/winterthings/entity/BaseBlockEntity.java
@@ -1,15 +1,19 @@
 package geek.winterthings.entity;
 
+import geek.winterthings.WTRegistries;
 import net.minecraft.core.BlockPos;
+import net.minecraft.nbt.CompoundTag;
 import net.minecraft.world.level.block.entity.BlockEntity;
-import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockState;
 
 public class BaseBlockEntity extends BlockEntity{
 
-    public BaseBlockEntity(BlockEntityType<?> p_155228_, BlockPos p_155229_, BlockState p_155230_) {
-        super(p_155228_, p_155229_, p_155230_);
-        //TODO Auto-generated constructor stub
+    public BaseBlockEntity(BlockPos p_155229_, BlockState p_155230_) {
+        super(WTRegistries.BASE_BLOCK_ENTITY.get(), p_155229_, p_155230_);
     }
-    
+    @Override
+    protected void saveAdditional(CompoundTag p_187471_) {
+        // TODO Auto-generated method stub
+        super.saveAdditional(p_187471_);
+    }
 }

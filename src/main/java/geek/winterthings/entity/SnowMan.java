ebd64481664c4cb4c40836036740e9b4d902f6bc
Robin Seifert <rseifert.phone@gmail.com>
2023-12-26 20:29:18 -0500

    adding generic ai movement code

--- src/main/java/geek/winterthings/entity/SnowMan.java
+++ src/main/java/geek/winterthings/entity/SnowMan.java
@@ -2,6 +2,14 @@ package geek.winterthings.entity;
 
 import net.minecraft.world.entity.ai.attributes.AttributeSupplier;
 import net.minecraft.world.entity.ai.attributes.Attributes;
+import net.minecraft.world.entity.ai.goal.*;
+import net.minecraft.world.entity.ai.goal.target.HurtByTargetGoal;
+import net.minecraft.world.entity.ai.goal.target.NearestAttackableTargetGoal;
+import net.minecraft.world.entity.animal.IronGolem;
+import net.minecraft.world.entity.animal.Turtle;
+import net.minecraft.world.entity.monster.ZombifiedPiglin;
+import net.minecraft.world.entity.npc.AbstractVillager;
+import net.minecraft.world.entity.player.Player;
 import org.jetbrains.annotations.ApiStatus.OverrideOnly;
 
 import net.minecraft.server.commands.TeamCommand;
@@ -16,6 +24,15 @@ public class SnowMan extends PathfinderMob {
     public SnowMan(EntityType<? extends PathfinderMob> p_21683_, Level p_21684_) {
         super(p_21683_, p_21684_);
         //TODO Auto-generated constructor stub
+    }
+
+    @Override
+    protected void registerGoals() {
+        this.goalSelector.addGoal(8, new LookAtPlayerGoal(this, Player.class, 8.0F));
+        this.goalSelector.addGoal(8, new RandomLookAroundGoal(this));
+        this.goalSelector.addGoal(7, new WaterAvoidingRandomStrollGoal(this, 1.0D));
+
+        this.targetSelector.addGoal(1, (new HurtByTargetGoal(this)));
     }
 
     @Override

1c6b41a5f7a790097ef21984ad14337379ce8727
LegendaryGeek <timothydaley4@gmail.com>
2023-12-27 13:47:56 -0500

    changed to supplier in attempt to fix, didnt

--- src/main/java/geek/winterthings/items/SnowmanSpawner.java
+++ src/main/java/geek/winterthings/items/SnowmanSpawner.java
@@ -1,5 +1,7 @@
 package geek.winterthings.items;
 
+import java.util.function.Supplier;
+
 import org.checkerframework.checker.nullness.qual.NonNull;
 
 import net.minecraft.server.level.ServerLevel;
@@ -7,21 +9,22 @@ import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResultHolder;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.Mob;
 import net.minecraft.world.entity.MobSpawnType;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.ClipContext;
 import net.minecraft.world.level.Level;
+import net.minecraftforge.common.ForgeSpawnEggItem;
 
 public class SnowmanSpawner extends Item {
 
     EntityType<?> entityToSpawn;
 
-    public SnowmanSpawner(Properties properties, @NonNull EntityType<?> entityToSpawn) {
+    public SnowmanSpawner(Properties properties, Supplier<? extends EntityType<? extends Mob>> type) {
         super(properties);
-        this.entityToSpawn = entityToSpawn;
-        //TODO Auto-generated constructor stub
+        entityToSpawn = type.get();
     }
 
     @Override
@@ -29,7 +32,7 @@ public class SnowmanSpawner extends Item {
         // TODO Auto-generated method stub
         super.onUseTick(level, entity, stack, p_41431_);
         if(entity instanceof Player player){
-            //SpawnEggItem
+            //ForgeSpawnEggItem
             entityToSpawn.spawn((ServerLevel)level, getPlayerPOVHitResult(level, player, ClipContext.Fluid.SOURCE_ONLY).getBlockPos(), MobSpawnType.SPAWN_EGG);
         }
     }
